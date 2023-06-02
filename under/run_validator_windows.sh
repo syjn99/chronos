fd68b0ed25457acbd44507d39cdac3224d067c8c
John <suub@superblock.xyz>
2023-06-02 13:24:30 +0900

    Add setup script for window

---
+++ under/run_validator_windows.sh
@@ -0,0 +1,3 @@
+BASEDIR=$(pwd)
+
+validator.exe -- --wallet-dir=$BASEDIR/wallet --chain-config-file=$BASEDIR/config.yml --config-file=$BASEDIR/config.yml --wallet-password-file=$BASEDIR/artifacts/wallet/password.txt
