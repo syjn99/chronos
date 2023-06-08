1d5181fbd2ec2c8ca5683bddf00e883ef096de02
John <suub@superblock.xyz>
2023-06-08 12:20:21 +0900

    Fix typo

--- under/setup_validator.bat
+++ under/setup_validator.bat
@@ -5,4 +5,4 @@ SET BASEDIR=%~dp0
 "%BASEDIR%..\validator.exe" wallet create --wallet-dir="%BASEDIR%wallet" --keymanager-kind=imported --wallet-password-file="%BASEDIR%artifacts/wallet/password.txt"
 
 @REM bazel run //cmd/validator:validator accounts import -- --wallet-dir=$BASEDIR/wallet --keys-dir=$BASEDIR/artifacts/keyfiles --wallet-password-file=$BASEDIR/artifacts/wallet/password.txt
-"%BASEDIR%..\validator.exe" accounts import --wallet-dir="%BASEDIR%%wallet" --keys-dir="%BASEDIR%artifacts/keyfiles" --wallet-password-file="%BASEDIR%artifacts/wallet/password.txt"
+"%BASEDIR%..\validator.exe" accounts import --wallet-dir="%BASEDIR%wallet" --keys-dir="%BASEDIR%artifacts/keyfiles" --wallet-password-file="%BASEDIR%artifacts/wallet/password.txt"
