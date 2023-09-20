f6023bf0ae34eeecbc79dcd1d2af5a72208b50b8
Jay <jay@superblock.xyz>
2023-09-20 18:24:45 +0900

    changed eth2 name to over & add to binary deployment (#41)

--- dist/build_chronos_osx_amd64.sh
+++ dist/build_chronos_osx_amd64.sh
@@ -1,7 +1,7 @@
-bazel build --config=osx_amd64 //cmd/beacon-chain //cmd/validator //cmd/prysmctl //tools/enr-calculator
+bazel build --config=osx_amd64 //cmd/beacon-chain //cmd/validator //cmd/prysmctl //tools/enr-calculator //tools/bootnode
 if [ $? -ne 0 ]; then
     echo "Bazel build failed."
     exit 1
 fi
-zip -j dist/chronos_osx_amd64.zip bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain bazel-bin/cmd/validator/validator_/validator bazel-bin/tools/enr-calculator/enr-calculator_/enr-calculator bazel-bin/cmd/prysmctl/prysmctl_/prysmctl
+zip -j dist/chronos_osx_amd64.zip bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain bazel-bin/cmd/validator/validator_/validator bazel-bin/tools/enr-calculator/enr-calculator_/enr-calculator bazel-bin/cmd/prysmctl/prysmctl_/prysmctl bazel-bin/tools/bootnode/bootnode_/bootnode
 rm -rf bazel-bin
--- dist/build_chronos_osx_arm64_minimal.sh
+++ dist/build_chronos_osx_arm64_minimal.sh
@@ -3,5 +3,5 @@ if [ $? -ne 0 ]; then
     echo "Bazel build failed."
     exit 1
 fi
-zip -j dist/chronos_osx_arm64_minimal.zip bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain bazel-bin/cmd/validator/validator_/validator bazel-bin/tools/enr-calculator/enr-calculator_/enr-calculator bazel-bin/cmd/prysmctl/prysmctl_/prysmctl
+zip -j dist/chronos_osx_arm64_minimal.zip bazel-bin/cmd/beacon-chain/beacon-chain_/beacon-chain bazel-bin/cmd/validator/validator_/validator bazel-bin/tools/enr-calculator/enr-calculator_/enr-calculator bazel-bin/cmd/prysmctl/prysmctl_/prysmctl bazel-bin/tools/bootnode/bootnode_/bootnode
 rm -rf bazel-bin
