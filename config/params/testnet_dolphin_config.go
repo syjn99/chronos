41220ed5691e49f66299ed0b6fdf96b96d82d09b
Jay <jay@superblock.xyz>
2024-06-24 17:14:44 +0900

    modified for dolphin launch (#139)

--- config/params/testnet_dolphin_config.go
+++ config/params/testnet_dolphin_config.go
@@ -4,8 +4,8 @@ package params
 func UseDolphinNetworkConfig() {
 	cfg := BeaconNetworkConfig().Copy()
 	cfg.BootstrapNodes = []string{
-		// Dolphin testnet boot nodes
-		"enr:-LG4QP-_uY01P5mD4-RrJzpuZlvLpyedCtkMU0q5Le69m60OY6sE5Bz0VI_592ujVjvxuvaJe_twOrpbq4GW0hTpXVGGAY_NUtwah2F0dG5ldHOIAAAAAAAAAACCaWSCdjSCaXCEIkBUf4RvdmVykPWl_UIAAAAA__________-Jc2VjcDI1NmsxoQOp3abawRE7r3d9Kjn7qKrBzMMqc5p8AVpb7a81_MYkcYN1ZHCCyyA",
+		// Dolphin testnet boot nodess
+		"enr:-LG4QBg_EHDI1NooVMFiODW81B6h5Xw1xGBQT8VfsR_IBZgdFZaGrmyFUomH-kd9gWOqaf_tlseyLRSXMZBsy2vMPWmGAZBJSMwth2F0dG5ldHOIAAAAAAAAAACCaWSCdjSCaXCEIkBUf4RvdmVykNBNsU8AAAAY__________-Jc2VjcDI1NmsxoQOaEHgEJPUhVaDcltNoWYFRCE7GcZccocBv6EW6uX1M2IN1ZHCCyyA",
 	}
 	OverrideBeaconNetworkConfig(cfg)
 }
@@ -18,11 +18,11 @@ func DolphinConfig() *BeaconChainConfig {
 	cfg.DepositChainID = 541762
 	cfg.DepositNetworkID = 541762
 	cfg.GenesisForkVersion = []byte{0x00, 0x00, 0x00, 0x28}
-	cfg.AltairForkEpoch = 2
+	cfg.AltairForkEpoch = 0
 	cfg.AltairForkVersion = []byte{0x01, 0x00, 0x00, 0x28}
-	cfg.BellatrixForkEpoch = 4
+	cfg.BellatrixForkEpoch = 0
 	cfg.BellatrixForkVersion = []byte{0x02, 0x00, 0x00, 0x28}
-	cfg.CapellaForkEpoch = 10
+	cfg.CapellaForkEpoch = 4
 	cfg.CapellaForkVersion = []byte{0x03, 0x00, 0x00, 0x28}
 	cfg.InitializeForkSchedule()
 	return cfg
